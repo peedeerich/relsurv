f4d05e2432edf10d8d3ecf19bbc86d6991edf39b
Paul Richards <sheff.pdr@gmail.com>
2016-03-07 10:15:37 +0000

    code to extract relative survival at given time point from curve. NOTE no type checking, and will report for nonsense times (negative, >max follow up)

---
+++ rs.extract.R
@@ -0,0 +1,19 @@
+# function to extract 5 (arbitrary?) year RS from an rs.surv object
+
+rs.extract <- function(rs.obj, yr=5){
+  
+  strata <- c(0,cumsum(rs.obj$strata))
+  rs     <- c()
+  for (i in 1:(length(strata)-1)){
+    rs.time <- rs.obj$time[strata[i]+1,strata[i+1]]
+    rs.surv <- rs.obj$surv[strata[i]+1,strata[i+1]]
+    rs[i] <- rs.surv[which.min(abs(rs.time-365.24*yr))]
+  }
+  
+  names(rs) <- names(rs.obj$strata)
+  rs
+  
+}
+
+
+#RS_pop_5yr <- RS_pop$surv[which.min(abs(RS_pop$time-365.24*5))]
